936bb0b773bb8d2ad958f486d6ad1338c732c82b
ysuzuki-bysystems <ysuzuki@bysystems.jp>
2023-11-09 16:18:24 +0900

    Update

---
+++ src/app/route.ts
@@ -0,0 +1,28 @@
+import { NextResponse } from "next/server";
+
+export async function GET() {
+  return NextResponse.json({
+    foods: [
+      {
+        name: "てんぷらうどん",
+        is_main_dish: true,
+      },
+      {
+        name: "だしまき卵",
+        is_main_dish: false,
+      },
+      {
+        name: "𩸽",
+        is_main_dish: false,
+      },
+      {
+        name: "グリーンピースごはん",
+        is_main_dish: null,
+      },
+      {
+        name: "ライスサラダ",
+        is_main_dish: false,
+      },
+    ],
+  });
+}
