67836916716e3f067a515cc012728f1825b3802c
ysuzuki-bysystems <ysuzuki@bysystems.jp>
2023-11-09 20:50:42 +0900

    Update

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,7 +1,38 @@
-import type { NextRequest } from 'next/server'
+import { NextResponse, type NextRequest } from 'next/server'
  
-export function middleware(request: NextRequest) {
-  console.log(request.method, request.url, JSON.stringify(Array.from(request.headers)));
+export async function middleware(request: NextRequest) {
+  const endpoint = process.env["INTROSPECT_ENDPOINT"];
+  const client_id = process.env["CLIENT_ID"];
+  const client_secret = process.env["CLIENT_SECRET"];
+  if (typeof endpoint === "undefined" || typeof client_id === "undefined" || typeof client_secret === "undefined") {
+    throw new Error(`${endpoint?.length}, ${client_id?.length}, ${client_secret?.length}`);
+  }
+
+  const token = request.headers.get("authorization");
+  if (token === null || !token.startsWith("Bearer ")) {
+    return NextResponse.json({ "error": "bad request" }, { status: 400 });
+  }
+
+  const response = await fetch(endpoint, {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/x-www-form-urlencoded",
+    },
+    body: new URLSearchParams({
+      token: token.slice("Bearer ".length),
+      client_id,
+      client_secret,
+    }).toString(),
+  });
+  if (!response.ok) {
+    console.warn(response.statusText, await response.text());
+    return NextResponse.json({ "error": "bad request" }, { status: 400 });
+  }
+
+  const raw = await response.json();
+  if (raw["active"] !== true) {
+    return NextResponse.json({ "error": "bad request" }, { status: 400 });
+  }
 }
  
 export const config = {
